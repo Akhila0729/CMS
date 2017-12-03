150499add504f7928db94a1df004eea919b5267d
NWMSU\S528767 <S528767@mail.nwmissouri.edu>
2017-12-02 21:45:19 -0600

    Signup&Login Functionality

--- app.js
+++ app.js
@@ -2,19 +2,35 @@ var Parse      = require("parse/node"),
 	path       = require("path"),
 	flash      = require("connect-flash"),
 	logger     = require("morgan"),
+	passport = require('passport');
+	ParseStrategy = require('passport-parse');
 	bodyParser = require("body-parser"),
 	express    = require("express"),
 	session    = require("express-session"),
-	http       = require("http").Server(app),
-	app        = express();
+	app        = express(),
+	http       = require("http").Server(app);
 //importing routes
 var indexRoutes = require("./routes/index"),
 	userRoutes  = require("./routes/user");
+
 //configuring parse database
 const appId  = "WZ7jM1yi3Do7Ekt0CZ9immXv4ahpKsK4vFiM5Lsf",
 	javakey  = "xv3hAAweUFPjdSoqbq1cZsQfmnZBjbxxtt0Ym3iv";
 Parse.initialize(appId,javakey);
 Parse.serverURL="https://parseapi.back4app.com/";
+
+var parseStrategy = new ParseStrategy({parseClient: Parse});
+// setting passport with the strategies
+passport.use(parseStrategy);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+app.use(passport.initialize());
+app.use(passport.session());
 //configuring application
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -28,7 +44,10 @@ app.use(session({
 app.set("views",path.resolve(__dirname,"views"));
 app.set("view engine","ejs");
 
+
+
 app.use(function(req,res,next){
+	res.locals.user = req.user;
 	res.locals.error   = req.flash("error");
 	res.locals.success = req.flash("success");
 	next();
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express'),
+	Parse   = require("parse/node"),
+	passport = require('passport'),
     router = express.Router();
 
+var userSession;
 // This method is called on launch of the application.
 router.get('/', function (req, res) {
     res.render('index', { title: 'childrenmonitor'});
@@ -10,12 +13,64 @@ router.get('/register', function (req, res) {
     res.render('signup', {title: 'Signup'});
 });
 
-router.get('/login', function (req, res) {
-    res.render('login', {title: 'Login'});
+router.post('/register', function (req, res) {
+    var user = new Parse.User();
+    user.set("username", req.body.user["username"]);
+	user.set("password", req.body.user["password"]);
+	user.set("email", req.body.user["email"]);
+	user.set("gender", req.body.user["gender"]);
+	user.set("firstName", req.body.user["fname"]);
+	user.set("lastName", req.body.user["lname"]);
+	user.set("phone",req.body.user["phone"]);
+	user.signUp(null, {
+	    success: function(result) {
+	      //Hooray! Let them use the app now.
+	      req.flash("success","successfully signed up...explore our application");
+	      res.redirect("/login");   
+	    },
+	    error: function(user, error) {
+	      // Show the error message somewhere and let the user try again.
+	      req.flash("error",error.code + " : " + error.message);
+	      res.redirect("back");
+	    }
+	});
 });
 
-router.post('/login', function(req, res){
-})
+router.get('/login', function (req, res) {
+    res.render('login');
+});
+
+router.post('/login',function(req,res){
+	Parse.User.logIn(req.body.username,req.body.password,{
+		success: function(user) {
+		    passport.authenticate('parse',function(err, user, info) {
+			    if (err) {
+			    	return res.status(400).json({payload : {error: info}, message : info.message});
+			 	}
+
+				if (!user) { 
+				   	return res.status(400).json({payload : {error: info}, message : info.message});
+				}
+
+				req.logIn(user, function(err) {
+					   	if (err) {
+					   		return res.status(400).json({payload : {error: err}, message : info.message});
+						}
+					 	res.render("user");
+						return res.json({
+					   		payload : req.user,
+					   		message : "Authentication successfull"
+					   	});
+					});
+				})(req,res);
+		},
+	    error: function(user, error) {
+			// The login failed. Check error to see why.
+			req.flash("error",error);
+			res.redirect("back");
+		}
+	});
+});
 
 router.get('/logout', function (req, res) {
 });
