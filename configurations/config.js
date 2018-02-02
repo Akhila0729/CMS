5976d7e3fdfb47ed62e8899138326fa9dd3f931e
Avinash Vasadi <S528770@mail.nwmissouri.edu>
2018-02-01 21:21:03 -0600

    Added Configurations to avoid hard coded values.

---
+++ configurations/config.js
@@ -0,0 +1,3 @@
+exports.Database_APP_ID = '77B2DD34-2AF4-9406-FF48-C2B184F01400';
+exports.Database_API_KEY = '617B6777-EE1C-2EFB-FF62-051909E63F00';
+exports.Database_server_URL = 'https://api.backendless.com';
